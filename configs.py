a134a8724e4e2b793af9dbfca77024506fbe6613
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-03-17 09:02:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     FSUB = getenv("FSUB", "Venom_Chatz")
     CHID = int(getenv("CHID", ""))
-    SUDO = list(map(int, getenv("SUDO", "6306738739").split()))
+    SUDO = list(map(int, getenv("SUDO", "5822700831").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
